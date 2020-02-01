1273d0777fcc9b5afd20b469a735a05e0ad48e43
James King <j@lostwith.me>
2020-02-01 13:48:37 +0000

    split p5 functions for better hot-reloading

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import * as P5 from 'p5';
+import P5 from 'p5/lib/p5.min';
+import setup from './setup';
+import draw from './draw';
 import './style.css';
 
 new P5(p5 => {
-  p5.setup = () => {
-    p5.createCanvas(window.innerWidth, window.innerHeight);
-    p5.background(40);
-  };
-
-  p5.draw = () => {};
+  p5.setup = () => setup(p5);
+  p5.draw = () => draw(p5);
 });
