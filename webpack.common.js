528d68314e95495d30e3916544afe816eef2b6fe
James King <james@wearecrystal.uk>
2020-02-27 16:58:48 +0000

    favicon files

--- webpack.common.js
+++ webpack.common.js
@@ -29,6 +29,20 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: false,
       template: require('html-webpack-template'),
+      lang: 'en-GB',
+      title: 'lost with me',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'msapplication-TileColor', content: '#030303' },
+        { name: 'theme-color', content: '#030303' },
+      ],
+      links: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#030303' },
+      ],
     }),
   ],
 
